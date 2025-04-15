89ad82167b2f3265cf3074dc2ba7f72f84e280dc
Stéffano Bonaiva Batista <steffano@outlook.com>
2025-04-15 11:14:12 -0300

    test: adding unit tests

--- internal/domain/errors_test.go
+++ internal/domain/errors_test.go
@@ -36,3 +36,46 @@ func TestNewVersionNotAvailableError(t *testing.T) {
 	assert.Equal(t, 1, baseErr.Code)
 	assert.Equal(t, "Error: go version \"1.16.0\" is not available Code: 1", err.Error())
 }
+
+func TestNewNoUpdatesAvailableError(t *testing.T) {
+	// Arrange
+	version := "1.16.0"
+	strategy := PatchStrategy
+
+	// Act
+	err := NewNoUpdatesAvailableError(strategy, version)
+
+	// Assert
+	baseErr, ok := err.(*baseError)
+	assert.True(t, ok)
+	assert.Equal(t, fmt.Sprintf(errMessageNoUpdatesAvailable, string(strategy), version), baseErr.Message)
+	assert.Equal(t, 1, baseErr.Code)
+	assert.Equal(t, "Error: no patch updates available for version \"1.16.0\" Code: 1", err.Error())
+}
+
+func TestNewNoGoInstallationsFoundError(t *testing.T) {
+	// Act
+	err := NewNoGoInstallationsFoundError()
+
+	// Assert
+	baseErr, ok := err.(*baseError)
+	assert.True(t, ok)
+	assert.Equal(t, errMessageNoGoInstallationsFound, baseErr.Message)
+	assert.Equal(t, 1, baseErr.Code)
+	assert.Equal(t, "Error: no go installations found Code: 1", err.Error())
+}
+
+func TestNewInvalidUpdateStrategyError(t *testing.T) {
+	// Arrange
+	strategy := MajorStrategy
+
+	// Act
+	err := NewInvalidUpdateStrategyError(strategy)
+
+	// Assert
+	baseErr, ok := err.(*baseError)
+	assert.True(t, ok)
+	assert.Equal(t, fmt.Sprintf(errMessageInvalidUpdateStrategy, string(strategy)), baseErr.Message)
+	assert.Equal(t, 1, baseErr.Code)
+	assert.Equal(t, "Error: \"major\" is not a valid update strategy Code: 1", err.Error())
+}
